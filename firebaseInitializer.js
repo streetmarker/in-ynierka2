1fb71bcb8b6df43f0df51c61fdae71da49ec25d0
Michał Jaroń <mjaron4@gmail.com>
2024-04-11 23:45:56 +0200

    mess auth paypal

--- firebaseInitializer.js
+++ firebaseInitializer.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import firebase from 'firebase/compat/app';
 import * as firebaseui from 'firebaseui'
 import 'firebaseui/dist/firebaseui.css'
@@ -53,6 +53,7 @@ ui.start('#firebaseui-auth-container', {
         photo: authResult.user.photoURL,
         provider: authResult.user.providerData.providerId
       }
+      console.log(redirectUrl);
       store.commit('setUser', formatData);
       // console.log(JSON.stringify(authResult, null, 2)+'||||'+redirectUrl);
 
@@ -60,48 +61,48 @@ ui.start('#firebaseui-auth-container', {
     },
     // signInFailure callback must be provided to handle merge conflicts which
     // occur when an existing credential is linked to an anonymous user.
-    signInFailure: function(error) {
-      // For merge conflicts, the error.code will be
-      // 'firebaseui/anonymous-upgrade-merge-conflict'.
-      if (error.code != 'firebaseui/anonymous-upgrade-merge-conflict') {
-        return Promise.resolve();
-      }
-      // The credential the user tried to sign in with.
-      var cred = error.credential;
-      // If using Firebase Realtime Database. The anonymous user data has to be
-      // copied to the non-anonymous user.
-      var app = firebase.app();
-      // Save anonymous user data first.
-      return app.database().ref('users/' + firebase.auth().currentUser.uid)
-          .once('value')
-          .then(function(snapshot) {
-            data = snapshot.val();
-            // This will trigger onAuthStateChanged listener which
-            // could trigger a redirect to another page.
-            // Ensure the upgrade flow is not interrupted by that callback
-            // and that this is given enough time to complete before
-            // redirection.
-            return firebase.auth().signInWithCredential(cred);
-          })
-          .then(function(user) {
-            // Original Anonymous Auth instance now has the new user.
-            return app.database().ref('users/' + user.uid).set(data);
-          })
-          .then(function() {
-            // Delete anonymnous user.
-            return anonymousUser.delete();
-          }).then(function() {
-            // Clear data in case a new user signs in, and the state change
-            // triggers.
-            data = null;
-            // FirebaseUI will reset and the UI cleared when this promise
-            // resolves.
-            // signInSuccessWithAuthResult will not run. Successful sign-in
-            // logic has to be run explicitly.
-            window.location.assign('<url-to-redirect-to-on-success>');
-          });
+    // signInFailure: function(error) {
+    //   // For merge conflicts, the error.code will be
+    //   // 'firebaseui/anonymous-upgrade-merge-conflict'.
+    //   if (error.code != 'firebaseui/anonymous-upgrade-merge-conflict') {
+    //     return Promise.resolve();
+    //   }
+    //   // The credential the user tried to sign in with.
+    //   var cred = error.credential;
+    //   // If using Firebase Realtime Database. The anonymous user data has to be
+    //   // copied to the non-anonymous user.
+    //   var app = firebase.app();
+    //   // Save anonymous user data first.
+    //   return app.database().ref('users/' + firebase.auth().currentUser.uid)
+    //       .once('value')
+    //       .then(function(snapshot) {
+    //         data = snapshot.val();
+    //         // This will trigger onAuthStateChanged listener which
+    //         // could trigger a redirect to another page.
+    //         // Ensure the upgrade flow is not interrupted by that callback
+    //         // and that this is given enough time to complete before
+    //         // redirection.
+    //         return firebase.auth().signInWithCredential(cred);
+    //       })
+    //       .then(function(user) {
+    //         // Original Anonymous Auth instance now has the new user.
+    //         return app.database().ref('users/' + user.uid).set(data);
+    //       })
+    //       .then(function() {
+    //         // Delete anonymnous user.
+    //         return anonymousUser.delete();
+    //       }).then(function() {
+    //         // Clear data in case a new user signs in, and the state change
+    //         // triggers.
+    //         data = null;
+    //         // FirebaseUI will reset and the UI cleared when this promise
+    //         // resolves.
+    //         // signInSuccessWithAuthResult will not run. Successful sign-in
+    //         // logic has to be run explicitly.
+    //         window.location.assign('<url-to-redirect-to-on-success>');
+    //       });
 
-    }
+    // }
   }
 });
 
